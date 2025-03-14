251e9b32c48cb7920d8d15afea93b5058553ccac
saranyamr <saranyaprem246@gmail.com>
2025-03-14 15:46:40 +0530

    TestStudent

---
+++ TestStudent.java
@@ -0,0 +1,14 @@
+class student{
+    int id;
+    String name;
+}
+public class TestStudent
+{
+    public static void main(String[] args) 
+    {
+        student s1=new student();
+        s1.id=102;
+        System.out.println(s1.id);
+    }
+    }
+
