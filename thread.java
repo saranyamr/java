c7e43337d40664b237b6adb28a6a775bc299365d
saranyamr <saranyaprem246@gmail.com>
2025-03-14 14:18:26 +0530

    thread

---
+++ thread.java
@@ -0,0 +1,7 @@
+public class thread{
+    public static void main(String[] args){
+        int[] [] numbers={{1,2,3},{1,5,6},{4,8,2}};
+        System.out.println(numbers[2][1]);
+        System.out.println(numbers[1][0]);
+    }
+}
