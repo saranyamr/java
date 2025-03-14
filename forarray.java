8d49a04fd5c8800892337ffb38baf694e74bb604
saranyamr <saranyaprem246@gmail.com>
2025-03-14 15:09:00 +0530

    forarray

---
+++ forarray.java
@@ -0,0 +1,11 @@
+public class forarray{
+    public static void main(String[] args){
+        String[] fruits={"apple","mango","guave","Strawberry"};
+        for(int i=0;i<=3;i++){
+
+        
+        System.out.println(fruits[i]);
+        }
+
+    }
+}
