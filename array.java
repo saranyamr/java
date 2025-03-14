e808adff015f1755cc5d61a4c4b2bcc3a00ed0e1
saranyamr <saranyaprem246@gmail.com>
2025-03-14 13:58:24 +0530

    array

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array{
+    public static void main(String[] args)
+        {
+            String[] flower={"jasmine","lotus","roja","sunflower"};
+            System.out.println(flower[1]);
+            System.out.println(flower[2]);
+            System.out.println(flower[3]);
+        }
+    }
