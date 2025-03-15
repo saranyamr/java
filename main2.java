1d68f8ea967c8724e2b288cb65166bf5f4f065d2
saranyamr <saranyaprem246@gmail.com>
2025-03-15 10:08:17 +0530

    main2

---
+++ main2.java
@@ -0,0 +1,40 @@
+class Vehicle{
+    public void drive()
+    {
+        System.out.println("Repairing a vehicle:");
+        }}
+        class Car extends Vehicle{
+            public void main(){
+                System.out.println("Repairing a car:");
+            }
+        }
+                  public class main2{
+            public static void main(String[] args){
+                Vehicle vehicle=new Vehicle();
+                Car car=new Car();
+                vehicle.drive();
+                car.drive();
+            }
+        }
+        
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
