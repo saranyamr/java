0234b48abc4779ac8a16bfb19aa8d71aca40a92c
saranyamr <saranyaprem246@gmail.com>
2025-03-15 09:44:30 +0530

    swap2

---
+++ swap2.java
@@ -0,0 +1,13 @@
+public class swap2{
+    public static void main(String[] args){
+        int a=20;
+        int b=30;
+System.out.println("Before Swapping:"+a);
+System.out.println("Before Swapping:"+b);
+int temp=a;
+a=b;
+b=temp;
+System.out.println("After Swapping:"+a);
+System.out.println("After Swapping:"+b);
+    }
+}
